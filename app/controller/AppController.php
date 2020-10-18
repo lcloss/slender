125d88ef04c7ba775e91a26cf4f00868cc81d06d
Luciano Closs <lcloss@gmail.com>
2020-10-18 22:06:47 +0100

    Initial commit

--- app/controller/AppController.php
+++ app/controller/AppController.php
@@ -3,8 +3,13 @@ namespace App\Controller;
 
 class AppController extends Controller
 {
-    public function index() {
+    public function __construct()
+    {
         parent::__construct();
+    }
+    
+    public function index() 
+    {
         echo $this->app->view('index');
     }
 }
--- helpers/functions.php
+++ helpers/functions.php
@@ -29,3 +29,45 @@ if ( !function_exists('endsWith') ) {
         }
     }
 }
+
+if ( !function_exists('randomStr')) {
+    function randomStr($length = 8, $params = ['lower', 'capital', 'number', 'symbol', 'simsym']) 
+    {
+        $low_letters = "abcdefghijklmnopqrstuvwxyz";
+        $cap_letters = strtoupper($low_letters);
+        $symbols = "!?#$%&-_";
+        $simple_symbols = "-_";
+        $numbers = "1234567890";
+    
+        $source = '';
+        if ( in_array('lower', $params) ) {
+            $source .= $low_letters;
+        }
+        if ( in_array('capital', $params) ) {
+            $source .= $cap_letters;
+        }
+        if ( in_array('number', $params) ) {
+            $source .= $numbers;
+        }
+    
+        if ( in_array('symbol', $params) ) {
+            $source .= $symbols;
+    
+        } elseif ( in_array('simsymb', $params) ) {
+            $source .= $simple_symbols;
+        }
+    
+        $max = strlen($source);
+        $i = 0;
+        $word = "";
+    
+        while ($i < $length) {
+            $num = rand() % $max;
+            $char = substr($source, $num, 1);
+            $word .= $char;
+            $i++;
+        }
+    
+        return $word;
+    }    
+}
